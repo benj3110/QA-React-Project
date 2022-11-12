d25352e5eb279ab1b10eca0981d81be61fc55328
benj3110 <benitovark@gmail.com>
2022-11-12 16:08:43 +0000

    basic navbar

--- my-app/src/Components/NavBar.jsx
+++ my-app/src/Components/NavBar.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import React from "react";
+import Home from "./Home";
+import Profile from "./Profile";
+
 
 const NavBar = () => {
-  return <nav></nav>;
+  return (
+    <Router>
+      <nav>
+        <Link className="LinkClass" to = "/"> Home </Link>
+        <Link className="LinkClass" to = "/Profile"> Profile </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default NavBar;
